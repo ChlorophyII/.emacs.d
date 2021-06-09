6bf83a293bb29ef47ff68e8d4512c14180f54c1e
ChlorophyII <shiyue9621@gmail.com>
2021-06-09 14:23:40 -0400

    Optimize start-up time

--- init.el
+++ init.el
@@ -4,6 +4,13 @@
 ;;    GUI will fail to install some packages
 ;;; Code:
 
+;; =============================================================================
+
+; To speed up start-up
+(setq gc-cons-threshold 50000000)
+
+;; =============================================================================
+
 (require 'package)
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
                     (not (gnutls-available-p))))
@@ -14,42 +21,20 @@
     ;; For important compatibility libraries like cl-lib
     (add-to-list 'package-archives
 		 '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
-(package-initialize)
+(if (version< emacs-version "27")
+    (package-initialize))
 
 ;; =============================================================================
-
-;; To autoload bash init file
-(shell-command "source ~/.bashrc")
-
-;; ChezScheme is ChlorophyII's default option
-(defvar scheme-program-name "chez")
-
-(defvar python-shell-interpreter "python3")
-
-;; =============================================================================
-
-;; Notice the slash after "texbin"
-(setenv "PATH" (concat
-		"/usr/local/bin:"
-		"/usr/local/sbin:"
-		"/Library/TeX/texbin/:"
-		(getenv "PATH")))
-(setq exec-path (split-string (getenv "PATH") path-separator))
-
-;; =============================================================================
-
-;; To avoid a stupid bug of package-install caused by empty archive
-(unless package-archive-contents
-  (package-refresh-contents))
 
 (unless (package-installed-p 'use-package)
+  (package-refresh-contents)
   (package-install 'use-package))
 
 ;; =============================================================================
 
-(unless (package-installed-p 'zenburn-theme)
-  (package-install 'zenburn-theme))
-(load-theme 'zenburn t)
+(use-package zenburn-theme
+  :ensure t
+  :config (load-theme 'zenburn t))
 
 (set-cursor-color "#7F9F7F") ; zenburn-green
 (setq-default cursor-type 'box) ; bar
@@ -97,29 +82,32 @@
 
 ;; =============================================================================
 
-;; Warning: Don't touch the code below
-;; AUCTeX will not function even a single character is missed
-
-(unless (package-installed-p 'auctex)
-  (package-install 'auctex))
-(require 'tex)
-
-;; To use AUCTeX preview, gs is needed
-(cond ((eq system-type 'darwin)
-       (setq preview-gs-command "/usr/local/bin/gs"))
-      ((eq system-type 'gnu/linux)
-       (setq preview-gs-command "/usr/bin/gs")))
-
-(defvar preview-auto-cache-preamble t)
-
-(with-eval-after-load "latex"
-  (add-to-list 'LaTeX-verbatim-macros-with-braces "hphantom")
-  (add-to-list 'LaTeX-verbatim-macros-with-braces "vphantom"))
-
-(setq TeX-PDF-mode t)
+(use-package tex
+  ;; Integrated environment for *TeX*
+  :ensure auctex
+  :defer t
+  :config
+  ;; To use AUCTeX preview, gs is needed
+  (cond ((eq system-type 'darwin)
+	 (setq preview-gs-command "/usr/local/bin/gs"))
+	((eq system-type 'gnu/linux)
+	 (setq preview-gs-command "/usr/bin/gs")))
+  ;; Notice the slash after "texbin"
+  (setenv "PATH" (concat
+		  "/usr/local/bin:"
+		  "/usr/local/sbin:"
+		  "/Library/TeX/texbin/:"
+		  (getenv "PATH")))
+  (setq exec-path (split-string (getenv "PATH") path-separator))
+  (defvar preview-auto-cache-preamble t)
+  (with-eval-after-load "latex"
+    (add-to-list 'LaTeX-verbatim-macros-with-braces "hphantom")
+    (add-to-list 'LaTeX-verbatim-macros-with-braces "vphantom"))
+  (setq TeX-PDF-mode t))
 
 (use-package latex-extra ; Adds several useful functionalities to LaTeX-mode.
   :ensure t
+  :defer t
   :hook ((LaTeX-mode . latex-extra-mode)
 	 (LaTeX-mode . turn-off-auto-fill))
   :init (add-hook 'LaTeX-mode-hook (lambda () (auto-fill-mode -1)) t)
@@ -128,11 +116,11 @@
 
 ;; =============================================================================
 
-;; Macaulay 2 start
-(load "~/.emacs-Macaulay2" t)
-;; Macaulay 2 end
-
-;; =============================================================================
+(use-package esup
+  ;; Benchmark Emacs Startup time without ever leaving your Emacs.
+  :ensure t
+  :defer t
+  :config (setq esup-depth 0))
 
 (use-package flycheck
   ;; On-the-fly syntax checking
@@ -155,6 +143,7 @@
 (use-package company-auctex
   ;; Company-mode auto-completion for AUCTeX
   :ensure t
+  :defer t
   :hook ((LaTeX-mode . company-auctex-init)
 	 (LaTeX-mode . (lambda ()
 			 (add-to-list (make-local-variable 'company-backends)
@@ -163,17 +152,12 @@
 (use-package company-math
   ;; Completion backends for unicode math symbols and latex tags
   :ensure t
+  :defer t
   :hook (LaTeX-mode . (lambda ()
 			(if (display-graphic-p)
 			    (add-to-list (make-local-variable 'company-backends)
 					 '(company-math-symbols-latex
 					   company-latex-commands))))))
-
-(use-package move-text
-  ;; Move current line or region with M-up or M-down.
-  :ensure t
-  :config
-  (move-text-default-bindings))
 
 (use-package smartparens
   ;; Automatic insertion, wrapping and paredit-like navigation with user defined pairs.
@@ -183,18 +167,47 @@
   (require 'smartparens-config)
   (sp-local-pair 'org-mode "$" "$"))
 
-(use-package all-the-icons
-  ;; A library for inserting Developer icons
+(use-package move-text
+  ;; Move current line or region with M-up or M-down.
   :ensure t
   :config
-  (unless (package-installed-p 'all-the-icons)
-    (all-the-icons-install-fonts "yes")))
+  (move-text-default-bindings))
+
+(use-package magit
+  ;; A Git porcelain inside Emacs.
+  :ensure t
+  :defer t)
+
+(use-package markdown-mode
+  ;; Major mode for Markdown-formatted text
+  :ensure t
+  :defer t)
+
+(use-package csv-mode
+  ;; Major mode for editing comma/char separated values
+  :ensure t
+  :defer t)
+
+(use-package pyvenv
+  ;; Python virtual environment interface
+  :ensure t
+  :defer t
+  :hook (python-mode . pyvenv-mode)
+  :config
+  (setenv "WORKON_HOME" "~/"))
 
 (use-package neotree
   ;; A tree plugin like NerdTree for Vim
   :ensure t
   :config
   (setq neo-theme 'icons))
+
+(use-package all-the-icons
+  ;; A library for inserting Developer icons
+  :ensure t
+  :config
+  (unless (package-installed-p 'all-the-icons)
+    (all-the-icons-install-fonts "yes")))
 
 (use-package fill-column-indicator
   ;; Graphically indicate the fill column
@@ -204,28 +217,10 @@
   (setq fci-rule-color "#4f4f4f")
   (setq fci-rule-column 80))
 
-(use-package magit
-  ;; A Git porcelain inside Emacs.
-  :ensure t)
-
-(use-package markdown-mode
-  ;; Major mode for Markdown-formatted text
-  :ensure t)
-
-(use-package csv-mode
-  ;; Major mode for editing comma/char separated values
-  :ensure t)
-
-(use-package pyvenv
-  ;; Python virtual environment interface
-  :ensure t
-  :hook (python-mode . pyvenv-mode)
-  :config
-  (setenv "WORKON_HOME" "~/"))
-
 (use-package wc-mode
   ;; Running word count with goals (minor mode)
   :ensure t
+  :defer t
   :hook text-mode
   :config
   (setq wc-modeline-format "WC[%w %tw %tc]"))
@@ -233,6 +228,7 @@
 (use-package writeroom-mode
   ;; Minor mode for distraction-free writing
   :ensure t
+  :defer t
   :hook ((writeroom-mode . (lambda ()
 			     (writeroom-adjust-width
 			      (truncate (- (* 0.52 (/ (display-pixel-width)
@@ -248,16 +244,19 @@
 (use-package highlight-parentheses
   ;; Highlight surrounding parentheses
   :ensure t
+  :defer t
   :hook (prog-mode . highlight-parentheses-mode))
 
 (use-package highlight-numbers
   ;; Highlight numbers in source code
   :ensure t
+  :defer t
   :hook (prog-mode . highlight-numbers-mode))
 
 (use-package fic-mode
   ;; Show FIXME/TODO/BUG(...) in special face only in comments and strings
   :ensure t
+  :defer t
   :hook ((prog-mode LaTeX-mode) . fic-mode))
 
 ;; =============================================================================
@@ -293,10 +292,25 @@
 
 ;; =============================================================================
 
+;; ChezScheme is ChlorophyII's default option
+(defvar scheme-program-name "chez")
+
+(defvar python-shell-interpreter "python3")
+
+;; Macaulay 2
+(load "~/.emacs-Macaulay2" t)
+
 (defun icloud ()
   "Change working directory to iCloud drive."
   (interactive)
   (cd "~/Library/Mobile Documents/com~apple~CloudDocs"))
+
+;; =============================================================================
+
+(run-with-idle-timer
+ 5 nil
+ (lambda ()
+   (setq gc-cons-threshold 1000000)))
 
 ;; =============================================================================
 

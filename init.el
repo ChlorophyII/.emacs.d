a672773ccbbcce638285753f2d4fe1b344174bd0
ChlorophyII <shiyue9621@gmail.com>
2022-07-09 15:41:30 -0400

    Update package-selected-packages

--- init.el
+++ init.el
@@ -400,7 +400,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(graphviz-dot-mode pyvenv all-the-icons ebib writeroom-mode fic-mode fill-column-indicator magit move-text multiple-cursors zenburn-theme wc-mode rainbow-mode markdown-mode impatient-mode neotree highlight-parentheses flycheck auctex use-package)))
+   '(graphql graphql-mode ess graphviz-dot-mode pyvenv all-the-icons ebib writeroom-mode fic-mode fill-column-indicator magit move-text multiple-cursors zenburn-theme wc-mode rainbow-mode markdown-mode impatient-mode neotree highlight-parentheses flycheck auctex use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

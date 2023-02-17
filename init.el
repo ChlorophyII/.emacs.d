142cea9b4ae49a3a640ae8984282ab26b792f249
ChlorophyII <shiyue9621@gmail.com>
2023-02-17 15:06:55 -0500

    Add tree-sitter mode for better syntax highlighting

--- init.el
+++ init.el
@@ -258,6 +258,18 @@
 				   '(nil nil) ;; no continuation indicators
 				   )))))
 
+(use-package tree-sitter
+  ;; Incremental parsing system
+  :ensure t
+  :config
+  (global-tree-sitter-mode)
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
+
+(use-package tree-sitter-langs
+  ;; Grammar bundle for tree-sitter
+  :ensure t
+  :defer t)
+
 (use-package highlight-parentheses
   ;; Highlight surrounding parentheses
   :ensure t
@@ -413,7 +425,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(graphql graphql-mode ess graphviz-dot-mode pyvenv all-the-icons ebib writeroom-mode fic-mode fill-column-indicator magit move-text multiple-cursors zenburn-theme wc-mode rainbow-mode markdown-mode impatient-mode neotree highlight-parentheses flycheck auctex use-package)))
+   '(tree-sitter-langs tree-sitter graphql graphql-mode ess graphviz-dot-mode pyvenv all-the-icons ebib writeroom-mode fic-mode fill-column-indicator magit move-text multiple-cursors zenburn-theme wc-mode rainbow-mode markdown-mode impatient-mode neotree highlight-parentheses flycheck auctex use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

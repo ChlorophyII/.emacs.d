f3bbf8ff4a1f68d2800e2cccbe84798fce5440ff
ChlorophyII <shiyue9621@gmail.com>
2023-02-18 01:04:54 -0500

    Replace fic-mode with hl-todo
    To resolve compatibility issue with tree-sitter

--- init.el
+++ init.el
@@ -285,11 +285,12 @@
   :defer t
   :hook (prog-mode . highlight-numbers-mode))
 
-(use-package fic-mode
-  ;; Show FIXME/TODO/BUG(...) in special face only in comments and strings
+(use-package hl-todo
+  ;; Highlight TODO and similar keywords
+  ;; TODO NEXT OKAY FAIL DONE TEMP XXX
   :ensure t
   :defer t
-  :hook ((prog-mode LaTeX-mode) . fic-mode))
+  :hook ((prog-mode LaTeX-mode) . hl-todo-mode))
 
 ;; =============================================================================
 
@@ -433,7 +434,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(tree-sitter-langs tree-sitter graphql graphql-mode ess graphviz-dot-mode pyvenv all-the-icons ebib writeroom-mode fic-mode fill-column-indicator magit move-text multiple-cursors zenburn-theme wc-mode rainbow-mode markdown-mode impatient-mode neotree highlight-parentheses flycheck auctex use-package)))
+   '(hl-todo comment-tags tree-sitter-langs tree-sitter graphql graphql-mode ess graphviz-dot-mode pyvenv all-the-icons ebib writeroom-mode fic-mode fill-column-indicator magit move-text multiple-cursors zenburn-theme wc-mode rainbow-mode markdown-mode impatient-mode neotree highlight-parentheses flycheck auctex use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
